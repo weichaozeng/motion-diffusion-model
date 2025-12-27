007e1c1f57125bcf54f9e35c0fe3071a1d018474
weichaozeng <zvc22@outlook.com>
2025-12-27 10:04:06 +0800

    update

--- data_loaders/hand/gigahands.py
+++ data_loaders/hand/gigahands.py
@@ -404,7 +404,7 @@ if __name__ == "__main__":
             y = geometry.rotation_6d_to_matrix(y)
             # y = geometry.matrix_to_axis_angle(geometry.rotation_6d_to_matrix(y))
             y_root = sample['ref_motion_root']
-            y_trans += y_root
+            # y_trans += y_root
             y_trans = x0_trans 
         else:
             x0_trans = torch.zeros_like(sample['inp'].permute(2, 0, 1)[:, -1, :3])
