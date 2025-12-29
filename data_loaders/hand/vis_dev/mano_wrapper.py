b5880453b8ec43363be9ceac9a996d886fb67d63
weichaozeng <zvc22@outlook.com>
2025-12-29 17:08:32 +0800

    update

--- data_loaders/hand/vis_dev/mano_wrapper.py
+++ data_loaders/hand/vis_dev/mano_wrapper.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+current_file = Path(__file__).resolve()
+root_dir = current_file.parent.parent.parent.parent 
+if str(root_dir) not in sys.path:
+    sys.path.insert(0, str(root_dir))
+
 import torch
 import numpy as np
 import pickle
