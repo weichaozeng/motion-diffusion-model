d5ef382e80f651c45caedb9ef5297438aaec6b66
weichaozeng <zvc22@outlook.com>
2025-12-30 11:15:19 +0800

    update

--- data_loaders/hand/vis_dev/mano_wrapper.py
+++ data_loaders/hand/vis_dev/mano_wrapper.py
@@ -299,9 +299,9 @@ if __name__ == "__main__":
             focal_length=[cam['K'][0][0, 0], cam['K'][0][1, 1]],
             camera_center=[cam['K'][0][0, 2], cam['K'][0][1, 2]],
             camera_pose=get_pyrender_pose(cam),
-            cam_t=[[0, 0, 0]], 
+            cam_t=np.asarray([[0, 0, 0]]), 
             render_res=[h, w], 
-            is_right=[1], 
+            is_right=np.asarray([1]), 
         )
 
         # y render
