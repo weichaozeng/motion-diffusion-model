2b0101800f50e9afd6a82c844ab090be838f4903
weichaozeng <zvc22@outlook.com>
2026-01-06 20:41:09 +0800

    update

--- diffusion/respace.py
+++ diffusion/respace.py
@@ -122,12 +122,12 @@ class _WrappedModel:
         self.rescale_timesteps = rescale_timesteps
         self.original_num_steps = original_num_steps
 
-    def __call__(self, x, ts, model_kwargs=None, **kwargs):
+    def __call__(self, x, ts, **kwargs):
         map_tensor = th.tensor(self.timestep_map, device=ts.device, dtype=ts.dtype)
         new_ts = map_tensor[ts]
         if self.rescale_timesteps:
             new_ts = new_ts.float() * (1000.0 / self.original_num_steps)
-        return self.model(x, new_ts, model_kwargs=model_kwargs, **kwargs)
+        return self.model(x, new_ts, **kwargs)
 
     def __getattr__(self, name, default=None):
         # this method is reached only if name is not in self.__dict__.
