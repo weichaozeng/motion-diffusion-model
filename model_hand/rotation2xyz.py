d083069920343d873571d649034b52b4fed6ea5b
weichaozeng <zvc22@outlook.com>
2026-02-23 13:39:05 +0800

    update

--- model_hand/rotation2xyz.py
+++ model_hand/rotation2xyz.py
@@ -7,7 +7,7 @@ class Rotation2xyz:
         self.device = device
         self.hand_model = MANO(device=device, num_pca_comps=6, use_pca=False, use_flat_mean=False,).eval().to(device)
 
-    def __call__(self, pose, pose_rep, beta, translation, root_translation, ff_rotmat, return_vertices=False, R_cam2world=None, R_adj=None, **kwargs):
+    def __call__(self, pose, pose_rep, beta, translation=None, root_translation=None, ff_rotmat=None, return_vertices=False, R_cam2world=None, R_adj=None, **kwargs):
 
         x_rotations = pose
 
@@ -30,17 +30,18 @@ class Rotation2xyz:
             ff_rotmat = ff_rotmat.unsqueeze(1).repeat(1, nframes, 1, 1)
 
         # pose
-        all_root_pose_mat = rotations[:, :, 0]
-        all_root_pose_mat = torch.matmul(ff_rotmat, all_root_pose_mat)
-        if R_cam2world is not None and R_adj is not None:
-            # R_total_T (B, F, 3, 3) = (R_c2w @ R_adj)^T = R_adj^T @ R_c2w^T
-            if len(R_cam2world.shape) == 3:
-                R_cam2world = R_cam2world.unsqueeze(1).repeat(1, nframes, 1, 1)
-            # R_adj (B, F, 3, 3)
-            R_total = torch.matmul(R_cam2world, R_adj)
-            R_total_inv = R_total.transpose(-1, -2) 
-            all_root_pose_mat = torch.matmul(R_total_inv, all_root_pose_mat)
-        rotations[:, :, 0] = all_root_pose_mat
+        if ff_rotmat is not None:
+            all_root_pose_mat = rotations[:, :, 0]
+            all_root_pose_mat = torch.matmul(ff_rotmat, all_root_pose_mat)
+            if R_cam2world is not None and R_adj is not None:
+                # R_total_T (B, F, 3, 3) = (R_c2w @ R_adj)^T = R_adj^T @ R_c2w^T
+                if len(R_cam2world.shape) == 3:
+                    R_cam2world = R_cam2world.unsqueeze(1).repeat(1, nframes, 1, 1)
+                # R_adj (B, F, 3, 3)
+                R_total = torch.matmul(R_cam2world, R_adj)
+                R_total_inv = R_total.transpose(-1, -2) 
+                all_root_pose_mat = torch.matmul(R_total_inv, all_root_pose_mat)
+            rotations[:, :, 0] = all_root_pose_mat
         B, F, J, _, _ = rotations.shape
         rotmat_flat = rotations.view(-1, J, 3, 3)
         rotvec_flat = geometry.matrix_to_axis_angle(rotmat_flat)
@@ -51,13 +52,15 @@ class Rotation2xyz:
 
 
         # trans
-        translation_world_rel = torch.matmul(ff_rotmat, translation.unsqueeze(-1)).squeeze(-1)
-        translation_world = translation_world_rel + root_translation.unsqueeze(1)
-        if R_cam2world is not None and R_adj is not None:
-            translation = torch.matmul(R_total_inv, translation_world_rel.unsqueeze(-1)).squeeze(-1)
-        else:
-            translation = translation_world
-        translation = translation.reshape(-1, 3)
+        if translation is not None:
+            if ff_rotmat is not None:
+                translation_world_rel = torch.matmul(ff_rotmat, translation.unsqueeze(-1)).squeeze(-1)
+                translation_world = translation_world_rel + root_translation.unsqueeze(1)
+                if R_cam2world is not None and R_adj is not None:
+                    translation = torch.matmul(R_total_inv, translation_world_rel.unsqueeze(-1)).squeeze(-1)
+                else:
+                    translation = translation_world
+            translation = translation.reshape(-1, 3)
 
         # shapes
         shapes = beta.unsqueeze(1).repeat(1, F, 1).view(-1, 10)
