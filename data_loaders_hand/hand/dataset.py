b70c3411f6ce0508bae876d963b8a5f7e0905c7e
weichaozeng <zvc22@outlook.com>
2026-02-23 17:01:43 +0800

    update

--- data_loaders_hand/hand/dataset.py
+++ data_loaders_hand/hand/dataset.py
@@ -169,7 +169,7 @@ class Dataset(torch.utils.data.Dataset):
             # corr
             'R_c2w': R_c2w.float(),
             'R_adj': R_adj.float(),
-            'f_scale_factor': f_scale_factor.float(),
+            'f_scale_factor': f_scale_factor.float().unsqueeze(0),
         }
 
         return data_dict
