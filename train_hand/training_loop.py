a822182afbf61b6ba48fd3156f03bdcab4e2b572
weichaozeng <zvc22@outlook.com>
2026-02-24 11:41:46 +0800

    update

--- train_hand/training_loop.py
+++ train_hand/training_loop.py
@@ -368,7 +368,7 @@ class TrainLoop:
                     beta=vis_sample['gt_beta'], 
                     ff_rotmat=vis_sample['y_ff_root_orient_rotmat'], 
                     translation=vis_sample['y_trans'], 
-                    root_translation=vis_sample['gt_root_trans'], 
+                    root_translation=vis_sample['y_root_trans'], 
                     return_vertices=True)
                 y_video_dir = os.path.join(vis_out_dir, 'ori_video')
                 vis_gigahands.render_video(y_verts, y_video_dir, rgb_video_paths, rgb_frame_indices, gt_cam, suffix_masks)
