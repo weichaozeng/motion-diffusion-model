cfe9ad94cb07ec3285e536ffbc706df0f9e7606a
weichaozeng <zvc22@outlook.com>
2026-02-24 12:58:39 +0800

    update

--- model_hand/rotation2xyz.py
+++ model_hand/rotation2xyz.py
@@ -62,8 +62,7 @@ class Rotation2xyz:
                     translation = torch.matmul(R_cam2world.transpose(-1, -2), translation_world.unsqueeze(-1)).squeeze(-1)
                 else:
                     translation = translation_world
-            
-
+            translation = translation.reshape(-1, 3)
             # 0224
             J_rest = torch.matmul(self.hand_model.J_regressor, self.hand_model.v_template)
             J0 = J_rest[0]
@@ -71,7 +70,7 @@ class Rotation2xyz:
             rot_J0 = torch.matmul(rot_matrix, J0.unsqueeze(-1)).squeeze(-1)
             translation -= rot_J0
 
-            translation = translation.reshape(-1, 3)
+            
 
         # shapes
         shapes = beta.unsqueeze(1).repeat(1, F, 1).view(-1, 10)
